e90abb72fe3d53460814669a47a52ea582b400c8
Arvid Sätterkvist <arvidsat@gmail.com>
2021-09-25 13:14:53 +0200

    fix: add 100% size to svg

--- src/components/links/svg/Email.tsx
+++ src/components/links/svg/Email.tsx
@@ -1,6 +1,9 @@
+import styles from "./Svg.module.css";
+
 function Email() {
   return (
     <svg
+      className={styles.Svg}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
